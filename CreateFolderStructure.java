45e31977edb4269146796f7bcb1ddc22533129a8
Thais Santos Damasceno <tssdamasceno@gmail.com>
2023-12-27 18:07:03 -0800

    add iteration to create directory inside parent

--- CreateFolderStructure.java
+++ CreateFolderStructure.java
@@ -8,6 +8,7 @@ public class CreateFolderStructure {
 
         System.out.print("Enter semester number: ");
         Integer semesterNumber = input.nextInt(); 
+        input.nextLine(); //consume the newline character
 
         String semesterDirectoryName = "SEM" + semesterNumber;
         File semesterDirectory = new File(semesterDirectoryName); 
@@ -15,13 +16,29 @@ public class CreateFolderStructure {
         if(!semesterDirectory.exists()) {
             semesterDirectory.mkdir(); 
         }
+
+        System.out.print("Enter how many courses are you taking this semester? (this will result in the number of folders created): "); 
+        Integer numberOfFolders = input.nextInt(); 
+        input.nextLine();
+        System.out.println();
+
+        for (int num = 1; num <= numberOfFolders; num++) {
+            System.out.print("Enter the name for folder " + num + ":");
+            String userCourseName = input.nextLine(); 
+
+            File courseDirectory = new File(semesterDirectoryName + File.separator + userCourseName); 
+
+            if(!courseDirectory.exists()) {
+                courseDirectory.mkdir(); 
+                //call method that create directory structure inside each course directory
+            }
+            System.out.println();
+        }
+
+
+
+
+
     }
-
-
-
-
-
-
-    
     
 }
